b96fc894581c9f3e6b9e7facd8d21e0060614a67
kimi.wang <kimi.wang@smyze.cn>
2022-09-16 18:07:04 +0800

    init

---
+++ data/constellations.js
@@ -0,0 +1,85 @@
+export const constellations = [{
+    "name": "白羊座",
+    "icon": "/images/cons/by.png",
+    "date": "3.21 ~ 4.19",
+    "sign": "Aries",
+    "symbol": "♈︎"
+  },
+  {
+    "name": "金牛座",
+    "icon": "/images/cons/jn.png",
+    "date": "4.20 ~ 5.20",
+    "sign": "Taurus",
+    "symbol": "♉︎"
+  },
+  {
+    "name": "双子座",
+    "icon": "/images/cons/shuangzi.png",
+    "date": "5.21 ~ 6.21",
+    "sign": "Gemini",
+    "symbol": "♊︎"
+  },
+  {
+    "name": "巨蟹座",
+    "icon": "/images/cons/jx.png",
+    "date": "6.22 ~ 7.22",
+    "sign": "Cancer",
+    "symbol": "♋︎"
+  },
+  {
+    "name": "狮子座",
+    "icon": "/images/cons/shizi.png",
+    "date": "7.23 ~ 8.22",
+    "sign": "Leo",
+    "symbol": "♌︎"
+  },
+  {
+    "name": "处女座",
+    "icon": "/images/cons/cn.png",
+    "date": "8.23 ~ 9.22",
+    "sign": "Virgo",
+    "symbol": "♍︎"
+  },
+  {
+    "name": "天秤座",
+    "icon": "/images/cons/tc.png",
+    "date": "9.23 ~ 10.23",
+    "sign": "Libra",
+    "symbol": "♎︎"
+  },
+  {
+    "name": "天蝎座",
+    "icon": "/images/cons/tx.png",
+    "date": "10.24 ~ 11.22",
+    "sign": "Scorpio",
+    "symbol": "♏︎"
+  },
+  {
+    "name": "射手座",
+    "icon": "/images/cons/ss.png",
+    "date": "11.23 ~ 12.21",
+    "sign": "Sagittarius",
+    "symbol": "♐︎"
+  },
+  {
+    "name": "摩羯座",
+    "icon": "/images/cons/mj.png",
+    "date": "12.22 ~ 1.19",
+    "sign": "Capricornus",
+    "symbol": "♑︎"
+  },
+  {
+    "name": "水瓶座",
+    "icon": "/images/cons/sp.png",
+    "date": "1.20 ~ 2.18",
+    "sign": "Aquarius",
+    "symbol": "♒︎"
+  },
+  {
+    "name": "双鱼座",
+    "icon": "/images/cons/sy.png",
+    "date": "2.19 ~ 3.20",
+    "sign": "Pisces",
+    "symbol": "♓︎"
+  }
+]
---
+++ main.js
@@ -0,0 +1,29 @@
+import App from './App'
+import store from './store'
+import i18n from './lang/i18n'
+
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+Vue.prototype.$store = store
+App.mpType = 'app'
+const app = new Vue({
+	i18n,
+	store,
+	...App
+})
+app.$mount()
+// #endif
+
+
+// #ifdef VUE3
+import {createSSRApp} from 'vue'
+
+export function createApp() {
+	const app = createSSRApp(App)
+	app.use(i18n)
+	app.use(store)
+	return {app}
+}
+// #endif
